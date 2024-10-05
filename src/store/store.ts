07e4eea0fb6e28c734061594a61ea90e6e5ca11c
jet_black_ninja <pramodkmr958@gmail.com>
2024-10-05 16:25:13 +0530

    shadcn components and redus stores

---
+++ src/store/store.ts
@@ -0,0 +1,30 @@
+import {configureStore} from '@reduxjs/toolkit';
+import authReducer from './auth-slice';
+import adminProductSlice from './admin/products-slice';
+import adminOrderSlice from './admin/order-slice';
+
+import shopProductSlice from "./shop/products-slice";
+import shopCartSlice from './shop/cart-slice';
+import shopAddressSlice from "./shop/address-slice";
+import shopOrderSlice from './shop/order-slice';
+import shopSearchSlice from './shop/search-slice';
+import shopReviewSlice from './shop/review-slice';
+
+import commonFeatureSlice from './common-slice';
+
+const store =configureStore ({
+    reducer:{
+        auth: authReducer,
+        adminProduct: adminProductSlice,
+        adminOrder: adminOrderSlice,
+        shopProduct: shopProductSlice,
+        shopCart: shopCartSlice,
+        shopAddress: shopAddressSlice,
+        shopOrder: shopOrderSlice,
+        shopSearch: shopSearchSlice,
+        shopReview: shopReviewSlice,
+        common: commonFeatureSlice,
+    },
+});
+ 
+export default store;
