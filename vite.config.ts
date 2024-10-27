910c0362b7c75692082a709875bcce5cc8de8ee6
jet_black_ninja <pramodkmr958@gmail.com>
2024-10-27 10:23:34 +0530

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': process.env.VITE_SERVER_URL, // Correct usage of environment variable
+      '/api': {
+        target:process.env.VITE_SERVER_URL,
+        changeOrigin: true,
+        secure: false
+      }, // Correct usage of environment variable
     },
   },
   plugins: [react()],
